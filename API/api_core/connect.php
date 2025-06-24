7586141eaec3fdc7a3b419739ed8241073e3272a
EdersonSS <166855651+EdersonHTT@users.noreply.github.com>
2025-06-24 16:46:13 -0300

    Add files via upload

--- API/api_core/connect.php
+++ API/api_core/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$base = "bancoteste";
-$user = "root";
-$password = "";
+$host = "bmavyltiljme8pvlxehm-mysql.services.clever-cloud.com";
+$base = "bmavyltiljme8pvlxehm";
+$user = "u9odj4jnnwe8t0w0";
+$password = "CiYOnlnLB4Hs4H2GhXpU";
 
 $mysqlConnect = new mysqli($host, $user, $password, $base);
 if ($mysqlConnect->connect_errno) {
